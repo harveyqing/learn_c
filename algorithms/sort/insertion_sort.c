86e129024d4c981aa066c45f9f28462b75ce6837
qingtian <harveyqing@gmail.com>
2015-02-05 18:52:34 +0800

    add: add insertion sort

---
+++ algorithms/sort/insertion_sort.c
@@ -0,0 +1,55 @@
+/* Insertion sort
+ *
+ * Implementation of insertion sort.
+ */
+
+#include <stdio.h>
+
+void printArray(int arr[], int size)
+{
+    for (int i=0; i < size; i++)
+        printf("%d ", arr[i]);
+
+    printf("\n");
+}
+
+void insertionSort(int arr[], int n)
+{
+    int key;
+
+    for (int i = 1; i < n; i++) {
+        key = arr[i];
+        int j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are
+         * greater than key, to one position ahead
+         * of there current position.
+         */
+        while (j >= 0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+
+        arr[j+1] = key;
+    }
+}
+
+void test()
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+}
+
+int main()
+{
+    printf("Insertion sort test...");
+    test();
+
+    getchar();
+    return 0;
+}
+
